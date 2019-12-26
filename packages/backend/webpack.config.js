8c800f03f4c4907cc30007d9a1ac8b0bab73f5d3
pehtoorismies <pehtoorismies@gmail.com>
2019-12-26 18:42:55 +0200

    Add sourcemap

--- packages/backend/webpack.config.js
+++ packages/backend/webpack.config.js
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require('webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = ({ mode }) => {
   return {
     mode,
-    devtool: mode === 'development' ? 'eval-cheap-module-source-maps' : 0,
+    devtool:
+      mode === 'development' ? 'eval-cheap-module-source-maps' : 'sourcemap',
     entry: {
       index: './src/index.ts',
       generateSchema: './src/generateSchema.ts',
@@ -19,6 +21,13 @@ module.exports = ({ mode }) => {
     externals: [
       nodeExternals({
         modulesDir: path.resolve(__dirname, '../../node_modules'),
+      }),
+    ],
+    plugins: [
+      new webpack.BannerPlugin({
+        banner: 'require("source-map-support").install();',
+        raw: true,
+        entryOnly: false,
       }),
     ],
     module: {
